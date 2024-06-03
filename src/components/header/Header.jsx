f4f079c9a7c9b0346212706ab6afd7394c0f1806
NojusPaulius <nojusp2002@gmail.com>
2024-06-03 21:24:45 +0300

    starting point
    default setup

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import './header.scss';
+const Header = () => {
+    return( //gali isvesti tik viena elementa
+    <header>
+        <h1>React app demo</h1>
+    </header>
+    )
+}
+
+export default Header
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,25 @@
+import Article from "../article/Article"
+
+const Main = () =>{
+    return(
+        <main>
+            <Article
+            title="Naujiena 1"
+            content="Puiki diena"
+            />
+            <Article
+            title="Naujiena 2"
+             content=" labaiPuiki diena"/>
+            <Article
+            title="Naujiena 3"
+            content="Puikiausia diena"
+            />
+            <Article
+            title="Naujiena 4"
+            content="Puikesnios dienos nera"
+            />
+        </main>
+    )
+}
+
+export default Main
